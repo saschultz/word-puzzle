14d5e8a278b6681b0ef8f93a580af2584a3f6752
Sara Schultz and Shane Stafford <sara@>
2017-02-21 13:32:43 -0800

    correct indentation in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,26 @@
 $(document).ready(function() {
   $("#form1").submit(function() {
-  event.preventDefault();
-  var userInputString = $("#userString").val();
-  var splitString = userInputString.split("");
-  for (var index = 0; index < splitString.length; index += 1) {
-    if(splitString[index] === "a" || splitString[index] === "e" || splitString[index] === "i" || splitString[index] === "o" || splitString[index] === "u") {
-      splitString[index] = "-";
+    event.preventDefault();
+    var userInputString = $("#userString").val();
+    var splitString = userInputString.split("");
+    for (var index = 0; index < splitString.length; index += 1) {
+      if(splitString[index] === "a" || splitString[index] === "e" || splitString[index] === "i" || splitString[index] === "o" || splitString[index] === "u") {
+        splitString[index] = "-";
+      }
     }
-  }
-  var userInputString2 = splitString.join("");
-  $("#form1").hide();
-  $("#result").text(userInputString2);
-  $("#form2").show();
-
-  $("#form2").submit(function() {
-  event.preventDefault();
-  var userResponse = $("#userAnswer").val();
-
-  if (userInputString === userResponse) {
-    $("#output").text("Great jorb yahoo");
-    }
-    else {
-    $("#output").text("Fail");
-    }
+    var userInputString2 = splitString.join("");
+    $("#form1").hide();
+    $("#result").text(userInputString2);
+    $("#form2").show();
+    $("#form2").submit(function() {
+      event.preventDefault();
+      var userResponse = $("#userAnswer").val();
+      if (userInputString === userResponse) {
+        $("#output").text("Great jorb yahoo");
+      }
+      else {
+        $("#output").text("Fail");
+      }
     });
   });
 });
