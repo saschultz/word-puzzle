7c58f31f65f8d10ddd18f59f03d8a8b5ad79eaf8
Sara Schultz and Shane Stafford <sara@>
2017-02-21 11:04:06 -0800

    initialize HTML file, add variables, and add if statement

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#form1").submit(function(event) {
+  event.preventDefault();
+  var userInputString = $("#userString").val();
+  var splitString = userInputString.split("");
+  for (var index = 0; index < splitString.length; index += 1) {
+    if(splitString[index] === "a" || splitString[index] === "e" || splitString[index] === "i" || splitString[index] === "o" || splitString[index] === "u") {
+      splitString[index] = "-";
+    }
+  }
+  $("#result").text(splitString);
+  // console.log(splitString);
+  });
+});
