692e0f73af0ee679ead7276307bd13ead02192ec
Sara Schultz and Shane Stafford <shane@>
2017-02-21 11:18:24 -0800

    add join method to return result to user as a string

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
       splitString[index] = "-";
     }
   }
-  $("#result").text(splitString);
+  userInputString = splitString.join("");
+  $("#result").text(userInputString);
   // console.log(splitString);
   });
 });
